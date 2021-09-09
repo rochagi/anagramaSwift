5e9ebc931e863f6f61a75eca2a34efbc08946a57
rochagi <grocha0507@gmail.com>
2021-09-09 09:14:59 -0300

    Initial Commit

---
+++ wordsProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wordsProj
+//
+//  Created by Giovanna Rocha on 09/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
